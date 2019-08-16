1fa25245c50a219b9e6fc4b32fccb79d696f0f77
chrisbrinkmann <brinkmcd@gmail.com>
2019-08-16 13:39:20 -0400

    Updates

---
+++ precache-manifest.c85ce0d0eae3c260e7c38f57e4d0a7e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8903e94ec80bb5d831c2b930d5f837cb",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "59d49a8330ebc98f25b7",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "177628f4c3b8eddfbfd2",
+    "url": "/robofriends/static/css/main.e8902495.chunk.css"
+  },
+  {
+    "revision": "59d49a8330ebc98f25b7",
+    "url": "/robofriends/static/js/2.bae3d14d.chunk.js"
+  },
+  {
+    "revision": "177628f4c3b8eddfbfd2",
+    "url": "/robofriends/static/js/main.d66628c7.chunk.js"
+  },
+  {
+    "revision": "fa64f21900e25e9ebf3b",
+    "url": "/robofriends/static/js/runtime~main.2c7325ee.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
